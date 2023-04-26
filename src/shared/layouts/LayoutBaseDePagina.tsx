cfeec63b8a2899fd3bdc69d4851760358d991ff4
Dylan <106843146+PhillDylan@users.noreply.github.com>
2023-04-26 23:57:00 +0000

    feat: barra de ferramentas certa e layout responsivo

--- src/shared/layouts/LayoutBaseDePagina.tsx
+++ src/shared/layouts/LayoutBaseDePagina.tsx
@@ -7,12 +7,13 @@ import { useAppDrawerContext } from "../contexts";
 interface ILayoutBaseDePaginaProps{
 	children : React.ReactNode
   titulo: string;
-  barraDeFerramentas: ReactNode;
+  barraDeFerramentas?: ReactNode;
 }
 
 export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({ children,  titulo, barraDeFerramentas }) => {
   
   const smDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
+  const mdDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
   
   const theme = useTheme();
   
@@ -20,7 +21,7 @@ export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({ childre
   
   return( 
     <Box height='100%' display='flex' flexDirection='column' gap={1}>
-        <Box padding={1} display='flex' alignItems='center' height={theme.spacing(12)} >
+        <Box padding={1} display='flex' alignItems='center' gap={1} height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)} >
              
              {smDown && (
              <IconButton color="primary" onClick={toggleDrawerOpen}>
@@ -28,14 +29,23 @@ export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({ childre
             </IconButton>
             )}
           
-          <Typography variant="h5">
+          <Typography 
+          overflow="hidden"
+          whiteSpace="nowrap"
+          textOverflow="ellipsis"
+          variant={smDown ? 'h5' : mdDown ? 'h4' : 'h3'}
+          >
             {titulo}
           </Typography>
         </Box>
+        
+        {barraDeFerramentas && (
         <Box>
-          Barra de ferramentas
+          {barraDeFerramentas}
         </Box>
-        <Box>
+        )}
+        
+        <Box flex={1} overflow='auto'>
           {children}
         </Box>
     </Box>
