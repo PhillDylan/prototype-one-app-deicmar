074a345d5ff266a15179f4470ce982c8dad07fff
Dylan <106843146+PhillDylan@users.noreply.github.com>
2023-04-26 23:40:25 +0000

    termino da criação do layout base mas tem que arrumar o barra de ferramentas

--- src/shared/layouts/LayoutBaseDePagina.tsx
+++ src/shared/layouts/LayoutBaseDePagina.tsx
@@ -1,23 +1,32 @@
-
-import { IconButton, Typography, useTheme, Icon } from "@mui/material";
+import { ReactNode } from "react";
+import { IconButton, Typography, useTheme, Icon, useMediaQuery, Theme } from "@mui/material";
 import { Box } from "@mui/system";
 import MenuIcon from "@mui/icons-material/Menu";
+import { useAppDrawerContext } from "../contexts";
 
 interface ILayoutBaseDePaginaProps{
 	children : React.ReactNode
   titulo: string;
+  barraDeFerramentas: ReactNode;
 }
 
-export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({ children,  titulo }) => {
+export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({ children,  titulo, barraDeFerramentas }) => {
+  
+  const smDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
+  
   const theme = useTheme();
+  
+  const { toggleDrawerOpen } = useAppDrawerContext();
   
   return( 
     <Box height='100%' display='flex' flexDirection='column' gap={1}>
         <Box padding={1} display='flex' alignItems='center' height={theme.spacing(12)} >
              
-             <IconButton color="primary">
+             {smDown && (
+             <IconButton color="primary" onClick={toggleDrawerOpen}>
               <MenuIcon />
             </IconButton>
+            )}
           
           <Typography variant="h5">
             {titulo}
