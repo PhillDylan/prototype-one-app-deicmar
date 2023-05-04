0d08cb194a2001fc035a8d634c79ed7906adf7c9
PhillDylan <dylan.pdcs@gmail.com>
2023-05-04 11:55:15 -0300

    feat: codigoo  com o facial 80% completo

--- src/shared/components/NewPost.jsx
+++ src/shared/components/NewPost.jsx
@@ -8,6 +8,7 @@ export const NewPost = ({ image }) => {
 
   const imgRef = useRef();
   const canvasRef = useRef();
+  const croppedCanvasRef = useRef();
 
   const handleImage = async () => {
     // Pass img to model
@@ -16,57 +17,89 @@ export const NewPost = ({ image }) => {
       new faceapi.TinyFaceDetectorOptions()
     );
     setFaces(detections.map((d) => Object.values(d.box)));
-
-    console.log(detections);
-    console.log(faces);
-  };
-
-  const enter = () => {
-    const ctx = canvasRef.current.getContext('2d');
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = 'blue';
-    faces.map((face) => ctx.strokeRect(...face));
   };
 
   useEffect(() => {
-    const loadModels = () => {
-      Promise.all([
+    const loadModels = async () => {
+      await Promise.all([
         faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-      //  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-      //  faceapi.nets.faceExpressionNet.loadFromUri('/models')
-      ])
-        .then(handleImage)
-        .catch((e) => console.log(e));
+        // faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+        // faceapi.nets.faceExpressionNet.loadFromUri('/models')
+      ]);
+
+      imgRef.current && handleImage();
     };
 
-    imgRef.current && loadModels();
+    loadModels();
   }, []);
 
-  const addFriend = (e) => {
-    setFriends((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  };
-  console.log(friends);
+  useEffect(() => {
+    const enter = () => {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext('2d');
+      if (ctx) {
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = 'blue';
+        faces.map((face) => ctx.strokeRect(...face));
+      }
+    };
+
+    const drawCropped = () => {
+      if (faces.length > 0) {
+        const [x, y, w, h] = faces[0];
+        const img = imgRef.current;
+        const canvas = croppedCanvasRef.current;
+        const ctx = canvas.getContext('2d');
+    
+        // redimensiona a imagem original para a resolução máxima necessária
+        const MAX_WIDTH = 1200;
+        const MAX_HEIGHT = 800;
+        const ratio = Math.min(MAX_WIDTH / w, MAX_HEIGHT / h);
+        const newWidth = Math.floor(w * ratio);
+        const newHeight = Math.floor(h * ratio);
+        canvas.width = newWidth;
+        canvas.height = newHeight;
+        ctx.drawImage(img, x - 20 , y - 120, w + 60 , h + 160, 0, 0, newWidth, newHeight);
+    
+        // converte a nova imagem em um objeto Image com várias resoluções
+        const blobUrl = canvas.toDataURL();
+        const srcset = `${blobUrl} 1x, ${blobUrl.replace(
+          'data:image/png',
+          'data:image/png 2x'
+        )} 2x`;
+        setFriends((prev) => ({ ...prev, face: { blobUrl, srcset } }));
+      }
+    };
+    
+
+    enter();
+    drawCropped();
+  }, [faces]);
 
   return (
     <div className="container">
-      <div className="left" style={{ width, height }}>
-        <img 
-        ref={imgRef} 
-        crossOrigin="anonymous" 
-        src={url} 
-        alt="" 
+          <div className={friends.face ? "left hidden" : "left"} style={{ width, height, display: friends.face ? "none" : "block" }}>
+            <img ref={imgRef} crossOrigin="anonymous" src={url} alt="" />
 
+            <canvas ref={canvasRef} width={width} height={height}></canvas>
+
+            <canvas ref={croppedCanvasRef} style={{ display: 'none' }}></canvas>
+          </div>
+
+      {friends.face ? (
+        <img
+          src={friends.face.blobUrl}
+          alt=""
+          srcSet={friends.face.srcset}
+          style={{ width, height }}
         />
-
-        <canvas
-          onMouseEnter ={enter}
-          ref={canvasRef}
-          width={width}             
-          height={height}
-        >
-        </canvas>
-      </div>
+      ) : (
+        <p>Sem rosto detectado</p>
+      )}
     </div>
   );
-};
 
+  
+
+
+};
